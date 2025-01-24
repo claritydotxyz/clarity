792de74c279ee2b256a08bd8077ef60b359239c2
Clarity <horngbartol@hotmail.com>
2025-01-24 20:06:18 +0300

    Create index.ts

---
+++ ui/assets/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as DashboardIcon } from './DashboardIcon';
+export { default as InsightIcon } from './InsightIcon';
+export { default as SettingsIcon } from './SettingsIcon';
+export { default as ProfileIcon } from './ProfileIcon';
+export { default as LogoutIcon } from './LogoutIcon';
+export { default as NotificationIcon } from './NotificationIcon';
+export { default as ChartIcon } from './ChartIcon';
+export { default as FilterIcon } from './FilterIcon';
