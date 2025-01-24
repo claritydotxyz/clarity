97ba4b28c9dd8894bce3d3f3b3f4a4187fdbd72d
Clarity <horngbartol@hotmail.com>
2025-01-24 20:02:48 +0300

    Create __init__.py

---
+++ ui/pages/__init__.py
@@ -0,0 +1,8 @@
+"""
+UI pages package containing React components for different pages of the application.
+"""
+from .dashboard import DashboardPage
+from .insights import InsightsPage
+from .settings import SettingsPage
+
+__all__ = ['DashboardPage', 'InsightsPage', 'SettingsPage']
